139b3e9a01db32d2ec0133e73097d08d54b499b8
Nikola Perisic <cathode.y2k@gmail.com>
2019-07-15 13:38:54 +0200

    Updates

---
+++ precache-manifest.dad21e5255e5cdd7306afcf7f38e5e45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c13c8659fded083f8b05afff8d08dc8",
+    "url": "/deposit-box/index.html"
+  },
+  {
+    "revision": "4dabbbbc7cca842a4dea",
+    "url": "/deposit-box/static/css/main.54e6814a.chunk.css"
+  },
+  {
+    "revision": "07244b0c4e3a0e629044",
+    "url": "/deposit-box/static/js/2.7a991706.chunk.js"
+  },
+  {
+    "revision": "4dabbbbc7cca842a4dea",
+    "url": "/deposit-box/static/js/main.52cafa7a.chunk.js"
+  },
+  {
+    "revision": "e4146bd47591717c50bd",
+    "url": "/deposit-box/static/js/runtime~main.b9062fa1.js"
+  },
+  {
+    "revision": "2bc260fbdf1726f6c691f49800e17a37",
+    "url": "/deposit-box/static/media/Beep-tone.2bc260fb.mp3"
+  },
+  {
+    "revision": "c9d839e116850fa655c1c6922889f9e8",
+    "url": "/deposit-box/static/media/beep-error.c9d839e1.mp3"
+  }
+]);
